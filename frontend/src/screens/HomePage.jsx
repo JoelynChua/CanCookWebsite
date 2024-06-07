f2556772e1829ab1bcc85d3edc1120190df380f0
itayush <20028056@myrp.edu.sg>
2024-06-07 20:57:34 +0800

    test modify homepage.js

--- frontend/src/screens/HomePage.jsx
+++ frontend/src/screens/HomePage.jsx
@@ -4,7 +4,7 @@ export default function HomePage() {
   return (
     <div className='flex justify-center items-center w-full h-screen'>
       <h1>Homepage</h1>
-      <p>testJoelyn</p>
+      <p>testHSU</p>
     </div>
   )
 
