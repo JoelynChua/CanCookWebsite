51ee08c3ac383b8dc642569c22dceac83b6730e4
JoelynChua <joelynschool@gmail.com>
2024-06-07 20:34:56 +0800

    test push to master

--- frontend/src/screens/HomePage.jsx
+++ frontend/src/screens/HomePage.jsx
@@ -4,6 +4,7 @@ export default function HomePage() {
   return (
     <div className='flex justify-center items-center w-full h-screen'>
       <h1>Homepage</h1>
+      <p>testJoelyn</p>
     </div>
   )
 
