ae7aaecc4e158c1ac81995b0d0b5993640785c4a
itayush <20028056@myrp.edu.sg>
2024-06-19 02:33:05 +0800

    finished signup page

--- frontend/src/screens/Signup.jsx
+++ frontend/src/screens/Signup.jsx
@@ -5,74 +5,77 @@ import { Link } from 'react-router-dom';
 const SignUp = () => {
   return (
     <div className="flex justify-center items-center min-h-screen bg-purple_main" >
-      <div className="flex flex-col md:flex-row rounded-lg shadow-lg max-w-4xl p-5 md:p-0 bg-beige">
+      <div className="flex flex-col md:flex-row rounded-3xl shadow-lg max-w-6xl max-h-6xl p-5 md:p-0 bg-beige_main">
         
         {/* Left Section */}
-        <div className=" flex flex-col justify-center items-center p-10 md:rounded-l-lg bg-pink_main">
-          <img src="/path/to/your/logo.png" alt="CanCook?" className="w-24 h-24 mb-4" />
-          <h1 className="text-4xl font-bold">CanCook?</h1>
-          <p className="mt-4 text-center">Want to cook something delicious with the ingredients you have? Let us help you!</p>
+        <div className=" flex flex-col justify-center items-center p-10  md:rounded-l-3xl bg-pink_main">
+          <img src="/icon.png" alt="CanCook?" className="w-25 h-25 mb-4" />
+          <h1 className="text-6xl text-textcolor font-baloo">CanCook?</h1>
+          <p className="mt-2 text-center font-overlock font-bold text-textcolor text-md">Want to cook something delicious with ingredients you have? 
+          Let us help you!</p>
         </div>
         
         {/* Right Section */}
-        <div className="flex flex-col justify-center items-center p-10 md:p-20 bg-cream-300 md:rounded-r-lg">
-          <h2 className="text-3xl font-bold mb-5">Welcome</h2>
-          <form className="w-full max-w-sm">
+        <div className="flex flex-col justify-center items-center w-full p-10 md:p-20 md:rounded-r-3xl">
+          <h2 className="text-3xl font-overlock text-textcolor font-bold mb-5">WELCOME</h2>
+          <form className="w-full max-w-lg">
             <div className="mb-4">
-              <label className="block text-sm font-bold mb-2" htmlFor="username">
+              <label className="block text-3sm font-bold mb-1 font-overlock text-textcolor" htmlFor="username">
                 Username
               </label>
               <input 
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
+                className="shadow border rounded w-full py-2 px-3 text-textcolor leading-tight bg-blue_main focus:outline-none focus:shadow-outline" 
                 id="username" 
                 type="text" 
-                placeholder="Username" 
               />
             </div>
             <div className="mb-4">
-              <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
+              <label className="block text-3sm font-bold mb-1 font-overlock text-textcolor" htmlFor="email">
                 Email
               </label>
               <input 
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
+                className="shadow border rounded w-full py-2 px-3 text-textcolor leading-tight bg-blue_main focus:outline-none focus:shadow-outline" 
                 id="email" 
                 type="email" 
-                placeholder="Email" 
+                
               />
             </div>
             <div className="mb-6">
-              <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+              <label className="block text-3sm font-bold mb-1 font-overlock text-textcolor" htmlFor="password">
                 Password
               </label>
               <input 
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" 
+                className="shadow border rounded w-full py-2 px-3 text-textcolor leading-tight bg-blue_main focus:outline-none focus:shadow-outline"  
                 id="password" 
                 type="password" 
-                placeholder="Password" 
+                
               />
             </div>
-            <div className="flex items-center justify-between">
+            <div className="flex items-center justify-center">
               <button 
-                className="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" 
+                className=" bg-orange_main hover:bg-orange-400 text-textcolor hover:text-green-900 py-2 px-4 rounded-3xl font-bold font-overlock" 
                 type="button"
+                
               >
                 Sign up
               </button>
             </div>
           </form>
           <div className="my-4 w-full flex justify-between items-center">
-            <hr className="w-2/5" />
-            <span className="text-gray-400">or</span>
-            <hr className="w-2/5" />
+            <hr className="w-2/5 bg-blue_main" />
+            <span className="text-textcolor">or</span>
+            <hr className="w-2/5  bg-blue_main" />
           </div>
-          <button 
-            className="bg-white border border-gray-300 hover:bg-gray-100 text-gray-700 font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline flex items-center"
+          <div className='flex items-center'>
+          <img src="/googlelogo.png" alt="Google" className="w-15 h-10" />
+          <div 
+            className=" text-textcolor font-bold underline decoration-sky-500/80 font-overlock"
           >
-            <img src="/path/to/google-icon.png" alt="Google" className="w-5 h-5 mr-2" />
             Sign up with Google
-          </button>
-          <p className="mt-4">
-            Already have an account? <Link to="/signin" className="text-orange_main">Sign in</Link>
+          </div>
+          </div>
+          <p className="mt-4 text-textcolor font-bold font-overlock">
+            Already have an account? <Link to="/signin" className=" text-textcolor font-overlock font-bold underline decoration-sky-500/80">Sign in</Link>
           </p>
         </div>
         
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -11,6 +11,7 @@
 module.exports = {
   content: [
       "./src/**/*.{js,jsx,ts,tsx}",
+      "./src/*.tff",
   ],
   theme: {
       extend: {
@@ -19,8 +20,13 @@ module.exports = {
           pink_main: '#F7D2DB', // Example secondary color
           beige_main: '#FFF9EC',
           purple_main:'#CABAF2',
-          orange_main:'#FA9E05'
+          orange_main:'#FA9E05',
+          blue_main: '#E8F2F6',
         }
+      },
+      fontFamily: {
+        'overlock': ['Overlock SC', 'sans-serif'],
+        'baloo': ['Baloo', 'sans-serif'],
       },
       screens: {
           sm: '640px',
