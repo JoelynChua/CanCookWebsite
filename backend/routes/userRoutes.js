2db0fa93cf85197cd70b2a87aea2817d0d919cb1
NG JIA WEN <87064232+JIAWEN-NG@users.noreply.github.com>
2024-06-28 03:28:58 +0800

    Update userRoutes.js
    adjusted the route

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,16 +1,18 @@
 const express = require('express');
+const cors = require('cors');
 const userController = require('../controllers/userController');
 const reviewController = require('../controllers/reviewsController');
 
-
+const app = express();
 const router = express.Router();
+app.use(cors());
 
 router.get('/users', userController.getUsers);
 router.post('/users', userController.addUser);
 
 //router.get('/reviews', reviewController.getAllReviews);
 //router.get('/reviews/:recipeID', reviewController.getReviewsByRecipeID);
-router.get('/recipes/:recipeID/reviews', reviewController.getReviewsByRecipeID);
+router.get('/temp/:recipeID/reviews', reviewController.getReviewsByRecipeID);
 router.post('/reviews', reviewController.postReview);
 router.put('/reviews/:reviewID', reviewController.editReview);
 
