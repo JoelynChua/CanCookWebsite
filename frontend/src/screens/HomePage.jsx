b704a9753fa690938f21b0523488bf5a1c95969c
itayush <20028056@myrp.edu.sg>
2024-07-08 16:43:46 +0800

    Revert "Merge branch 'manage_acc_page_sue' into filter_recipe_Joelyn"
    This reverts commit 03105a5b246c61bdabe951d5f8431bfea10319c3, reversing
    changes made to cf022c418b98a7b73c9f43b6d92236cecc38b363.

--- frontend/src/screens/HomePage.jsx
+++ frontend/src/screens/HomePage.jsx
@@ -43,7 +43,6 @@ export default function HomePage() {
 
 
   return (
-
     <div className="flex flex-col justify-center items-center w-full h-screen">
       {/* filters */}
       <Filter />
@@ -55,7 +54,6 @@ export default function HomePage() {
       {/* <div className="w-full flex justify-center mt-4">
         <Carousel />
       </div> */}
-
     </div>
 
   )
