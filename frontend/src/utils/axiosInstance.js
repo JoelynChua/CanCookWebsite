9c191ccef6bfa46f17d8c330792ea5f12c731257
JoelynChua <joelynschool@gmail.com>
2024-07-12 23:11:37 +0800

    Update axiosInstance.js

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: "can-cook-website-frontend-a5o88nlzk-joelynchuas-projects.vercel.app",
     headers: {"Content-Type" : "application/json"},
     withCredentials: false,
 });
