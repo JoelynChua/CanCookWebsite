62fc6676fa691a1699f922451dede2b70a5e8f51
NG JIA WEN <87064232+JIAWEN-NG@users.noreply.github.com>
2024-07-21 01:29:08 +0800

    Update recipeDetails.jsx
    include the reviewDisplay.jsx component

--- frontend/src/screens/recipeDetails.jsx
+++ frontend/src/screens/recipeDetails.jsx
@@ -1,9 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { getRecipeById } from "../services/recipeService";
+import { useAuth } from '../contexts/AuthContext';
+import ReviewsDisplay from '../components/reviewsDisplay'; 
+
 
 export default function RecipeDetails() {
     //const [recipe, setrecipe] = useState([]);
+    const { currentUser } = useAuth(); // Get the current user from the AuthContext
+    //const [showReviews, setShowReviews] = useState(false);
     const { id } = useParams();
     const [recipe, setRecipe] = useState(null);
 
@@ -29,28 +34,40 @@ export default function RecipeDetails() {
 
 
     return (
-        
-        <div className="flex items-center justify-center min-h-screen">
-            <div id={recipe._id}>
-                {/* Render = how React components output UI elements */}
-                {/* Render (return statement of the function component) your recipe details here, e.g., recipe name, cuisine, etc. */}
-                <p className="font-bold text-2xl">{recipe.recipeName}</p>
-                <br></br>
-                <img className="size-56 rounded-lg" src={recipe.image} />
-                <p>Cuisine: {recipe.cuisine}</p>
-                <p>Duration: {recipe.duration}</p>
-                <p>Serving Size: {recipe.servingSize}</p>
-                <p>Calories: {recipe.calories}</p>
-                <p>Ingredients: {recipe.ingredients.join(', ')}</p>
-                <p>Equipments: {recipe.equipments.join(', ')}</p>
-                <br></br>
-                {/* Steps to be printed in new line after every , */}
-                <p>Steps:</p>
-                {recipe.steps.map((step, stepIndex) => (
-                    <p key={stepIndex}>{step}</p>
-                ))}
+        <div>
+            <div className="flex items-center justify-center min-h-screen">
+                <div id={recipe._id}>
+                    {/* Render = how React components output UI elements */}
+                    {/* Render (return statement of the function component) your recipe details here, e.g., recipe name, cuisine, etc. */}
+                    <p className="font-bold text-2xl">{recipe.recipeName}</p>
+                    <br></br>
+                    <img className="size-56 rounded-lg" src={recipe.image} />
+                    <p>Cuisine: {recipe.cuisine}</p>
+                    <p>Duration: {recipe.duration}</p>
+                    <p>Serving Size: {recipe.servingSize}</p>
+                    <p>Calories: {recipe.calories}</p>
+                    <p>Ingredients: {recipe.ingredients.join(', ')}</p>
+                    <p>Equipments: {recipe.equipments.join(', ')}</p>
+                    <br></br>
+                    {/* Steps to be printed in new line after every , */}
+                    <p>Steps:</p>
+                    {recipe.steps.map((step, stepIndex) => (
+                        <p key={stepIndex}>{step}</p>
+                    ))}
+
+                </div>
             </div>
+            <div>
+            <ReviewsDisplay recipeID={id} userID={currentUser ? currentUser.uid : null} />
+            </div>
+
         </div>
+        
+        
+       
+
+        
+        
 
 
     )
