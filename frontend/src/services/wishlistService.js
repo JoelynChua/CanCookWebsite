8f78fc56d53180c7d4755f38328e5a97aa37101a
JoelynChua <joelynschool@gmail.com>
2024-07-21 05:28:56 +0800

    Update wishlistService.js

--- frontend/src/services/wishlistService.js
+++ frontend/src/services/wishlistService.js
@@ -5,12 +5,12 @@ const cloudURL = "https://can-cook-website-bw1l.vercel.app";
 const finalURL = cloudURL;
 
 export const getAllWishlists = async () => {
-    const response = await axios.get(`${finalURL}/wishlists`);
+    const response = await axios.get(`${finalURL}/api/wishlists`);
     return response.data;
   };
 
 export const getWishlistByUserID = async (userID) => {
-  const response = await axios.get(`${finalURL}/wishlists/user/${userID}`)
+  const response = await axios.get(`${finalURL}/api/wishlists/user/${userID}`)
   console.log(response.data);
   return response.data;
   };
@@ -18,10 +18,10 @@ export const getWishlistByUserID = async (userID) => {
 
 export const addWishlist = async (userID, recipeID) => {
   const newWishlist = { UserID: userID, RecipeID: recipeID };
-  const response = await axios.post(`${finalURL}/wishlists`, newWishlist);
+  const response = await axios.post(`${finalURL}/api/wishlists`, newWishlist);
   return response.data;
 };
 
 export const deleteWishlist = async (wishlistID) => {
-  const response = await axios.delete(`${finalURL}/wishlists/${wishlistID}`)
+  const response = await axios.delete(`${finalURL}/api/wishlists/${wishlistID}`)
 }
